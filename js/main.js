0ed75f59d36197a164b3c9389ab0bb86e080cc9d
Julia-prog <julia.guniversity@gmail.com>
2020-11-15 19:41:00 +0300

    add styles;

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+    { id: 1, title: 'Notebook', price: 20000 },
+    { id: 2, title: 'Mouse', price: 1500 },
+    { id: 3, title: 'Keyboard', price: 5000 },
+    { id: 4, title: 'Gamepad', price: 4500 },
 ];
 
 const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-};
+    return `
+    <div class="product-item">
+        <h3 class="product-item-title">${title}</h3>
+        <p class="product-item-price">${price}</p>
+        <button class="by-btn"> Добавить в корзину</button>
+    </div>`;
+}
 
-const catalogInit = (list) => {
-    const productList = list.map((item) => renderProduct(item.title, item.price));
-    console.log(productList);
-    document.querySelector('.products').innerHTML = productList;
-};
+const catalogInit = (list = products) => {
+    list.map((item) => document.querySelector('.products').innerHTML += renderProduct(item.title, item.price));
+}
 
-catalogInit(products);
+catalogInit();
